e09f08d6b6197d4c1ee67b1d1b0170634f58e6db
Akhambay <assyl.akhambay@gmail.com>
2023-11-25 18:22:58 +0600

    Add Generic views and Detailed views

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import MovieTheatre, Movies, ShowTimes
+from .serializers import MovieTheatreSerializers, MoviesSerializers, ShowTimesSerializers
 
-# Create your views here.
+
+class MovieTheatreList(generics.ListCreateAPIView):
+    queryset = MovieTheatre.objects.all()
+    serializer_class = MovieTheatreSerializers
+
+
+class MovieTheatreDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MovieTheatre.objects.all()
+    serializer_class = MovieTheatreSerializers
+
+
+class MoviesList(generics.ListCreateAPIView):
+    queryset = Movies.objects.all()
+    serializer_class = MoviesSerializers
+
+
+class MoviesDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Movies.objects.all()
+    serializer_class = MoviesSerializers
+
+
+class ShowTimesList(generics.ListCreateAPIView):
+    queryset = ShowTimes.objects.all()
+    serializer_class = ShowTimesSerializers
+
+
+class ShowTimesDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ShowTimes.objects.all()
+    serializer_class = ShowTimesSerializers
