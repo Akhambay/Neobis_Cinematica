8002d464b1e6afa59f63eccc9f9fd160311cdb7b
Akhambay <assyl.akhambay@gmail.com>
2023-11-26 18:53:28 +0600

    Update Models, Add Room model

--- movies/admin.py
+++ movies/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import MovieTheatre, Movies, ShowTimes
+from .models import MovieTheatre, Movies, ShowTimes, Room
 
 admin.site.register(MovieTheatre)
 admin.site.register(Movies)
 admin.site.register(ShowTimes)
+admin.site.register(Room)
+# admin.site.register(Seat)
--- movies/models.py
+++ movies/models.py
@@ -33,6 +33,7 @@ class Movies(models.Model):
     genre = models.CharField(max_length=10, choices=genre_choice)
     date_start = models.DateField()
     date_end = models.DateField()
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.id}: {self.title} - {self.genre}"
@@ -42,16 +43,50 @@ class Movies(models.Model):
         verbose_name_plural = "Фильмы"
 
 
+class Room(models.Model):
+    room_choice = (
+        ('IMAX 3D', 'IMAX 3D'),
+        ('Standard', 'Standard'),
+        ('Dolby Atmos', 'Dolby Atmos'),
+    )
+
+    cinema = models.ForeignKey(MovieTheatre, on_delete=models.CASCADE)
+    room_type = models.CharField(max_length=15, choices=room_choice)
+    seat_id = models.ManyToOneRel
+
+    def __str__(self):
+        return f"{self.id}: {self.cinema} - {self.room_type}"
+
+    class Meta:
+        verbose_name = "Зал"
+        verbose_name_plural = "Залы"
+
+
 class ShowTimes(models.Model):
     start_session = models.TimeField()
     end_session = models.TimeField()
     cinema = models.ForeignKey(MovieTheatre, on_delete=models.CASCADE)
     movie = models.ForeignKey(Movies, on_delete=models.CASCADE)
-    # rooms = models.ForeignKey(Room, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return f"{self.id}: {self.cinema} - {self.movie} | {self.start_session} - {self.end_session}"
+        return f"{self.id}: {self.cinema} - {self.movie} | {self.start_session} - {self.price} KZT"
 
     class Meta:
         verbose_name = "Сеанс"
         verbose_name_plural = "Сеансы"
+
+
+class Seat(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    row_number = models.PositiveIntegerField()
+    seat_number = models.PositiveIntegerField()
+    is_reserved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.row_number} ряд, {self.seat_number} место'
+
+    class Meta:
+        verbose_name = "Место"
+        verbose_name_plural = "Места"
